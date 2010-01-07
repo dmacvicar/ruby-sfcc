d9709293b7cf6e5471e12450b1b104b5a995132d
Duncan Mac-Vicar P <dmacvicar@suse.de>
2010-01-07 17:42:12 +0100

    add test for enumeration

---
+++ test/test_sfcc_cim_enumeration.rb
@@ -0,0 +1,24 @@
+
+require File.join(File.dirname(__FILE__), 'helper')
+require 'pp'
+
+class SfccCimEnumerationTest < SfccTestCase
+
+  context "enumeration of instances" do
+    setup do
+      setup_cim_client
+      op = Sfcc::Cim::ObjectPath.new("root/cimv2", "")
+      @enm = @client.query(op, "select * from CIM_ComputerSystem", "wql")
+    end
+    
+    should "be running" do
+      assert cimom_running?
+    end
+
+    should "be able to iterate twice" do
+      assert !@enm.to_a.empty?
+      assert !@enm.to_a.empty?
+    end    
+  end   
+end            
+
