c58b4b8bbf8100084b8e953d176bddd080651d86
Klaus Kämpf <kkaempf@suse.de>
2011-11-06 21:17:56 +0100

    Use cmciConnect to find cmpisfcc

--- ext/sfcc/extconf.rb
+++ ext/sfcc/extconf.rb
@@ -1,7 +1,11 @@
 require 'mkmf'
 # $CFLAGS = "#{$CFLAGS} -Werror"
 
-raise "Cannot find NewCIMCEnv() in libcmpisfcc" unless have_library('cmpisfcc', 'NewCIMCEnv')
+unless have_library('cmpisfcc', 'cmciConnect')
+  STDERR.puts "Cannot find cmciConnect() in libcmpisfcc"
+  STDERR.puts "Is sblim-sfcc-devel installed ?"
+end
+
 #find_header 'cimc.h', '/usr/include/cimc'
 find_header 'cmci.h', '/usr/include/CimClientLib'
 create_makefile('sfcc')
