7ee93097c21c1465b1bd9e74b0762d6e2c9453e4
Klaus Kämpf <kkaempf@suse.de>
2012-05-18 09:51:55 +0200

    Don't 'puts' in tests

--- test/test_sfcc.rb
+++ test/test_sfcc.rb
@@ -27,10 +27,13 @@ class BasicTest < SfccTestCase
       
       should "allow for query" do
         result = @client.query(@op, "select * from CIM_ComputerSystem", "wql")
+        count = 0
         result.each do |instance|
-          puts instance
+          assert instance
+          count += 1
         end
-          end
+        assert count > 0
+      end
             
       context "class names" do
         setup do
