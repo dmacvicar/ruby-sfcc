8cc9683e016d21d0000df42b9c39573c2c912979
Klaus Kämpf <kkaempf@suse.de>
2012-06-21 15:58:42 +0200

    Split lib/sfcc.rb into class-specific subfiles

---
+++ lib/sfcc/instance.rb
@@ -0,0 +1,52 @@
+# sfcc/instance.rb
+
+module Sfcc
+
+  module Cim
+    
+    class Instance
+      def classname
+        self.object_path.classname
+      end
+
+      # properties => Hash
+      #
+      # return a hash with all properties
+      def properties
+        ret = {}
+        each_property { |key, value| ret[key] = value }
+        ret
+      end
+
+      # qualifiers => Hash
+      #
+      # return a hash with all qualifiers
+      def qualifiers
+        ret = {}
+        each_qualifier { |key, value| ret[key] = value }
+        ret
+      end
+
+      # property_qualifiers(property_name) => Hash
+      #
+      # return a hash with all qualifiers for +property_name+
+      def property_qualifiers(property_name)
+        ret = {}
+        each_property_qualifier(property_name) { |key, value| ret[key] = value }
+        ret
+      end
+      
+      # returns the string representation of
+      # the object path for this instance
+      def to_s
+        object_path.to_s
+      end
+
+      def method_missing name, *args
+        self.property name
+      end
+    end
+
+  end # module Cim
+  
+end # module Sfcc
