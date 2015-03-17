cf91f0869c6dfd5290e90111b4449af3805d1428
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-03-17 13:23:21 +0100

    port to minitest/spec

--- test/test_sfcc.rb
+++ test/test_sfcc.rb
@@ -1,72 +1,71 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
-require 'pp'
+require_relative 'helper'
 
-class BasicTest < SfccTestCase
-
-  context "a running CIMOM" do
-    setup do
-      setup_cim_client
+describe "a running CIMOM" do
+  include SfccTestUtils
+  
+  before do
+    setup_cim_client
+  end
+  
+  it "should be running" do
+    assert cimom_running?
+  end
+  
+  it "should be of class Client" do
+    assert_kind_of(Sfcc::Cim::Client, @client)
+  end
+  
+  describe "a new object path for root/cimv2" do
+    before do
+      @op = Sfcc::Cim::ObjectPath.new("root/cimv2")
     end
-
-    should "be running" do
-      assert cimom_running?
+    
+    it "should be of class ObjectPath" do
+      assert_kind_of(Sfcc::Cim::ObjectPath, @op)
     end
-
-    should "be of class Client" do
-      assert_kind_of(Sfcc::Cim::Client, @client)
+    
+    it "should allow for query" do
+      result = @client.query(@op, "select * from CIM_ComputerSystem", "wql")
+      count = 0
+      result.each do |instance|
+        assert instance
+        count += 1
+      end
+      assert count > 0
     end
-        
-    context "a new object path for root/cimv2" do
-      setup do
-        @op = Sfcc::Cim::ObjectPath.new("root/cimv2")
+    
+    describe "class names" do
+      before do
+        @class_names = @client.class_names(@op, Sfcc::Flags::DeepInheritance)
       end
       
-      should "be of class ObjectPath" do
-        assert_kind_of(Sfcc::Cim::ObjectPath, @op)
+      it "should be a Cimc::Enumeration" do
+        assert_kind_of(Sfcc::Cim::Enumeration, @class_names)
       end
       
-      should "allow for query" do
-        result = @client.query(@op, "select * from CIM_ComputerSystem", "wql")
-        count = 0
-        result.each do |instance|
-          assert instance
-          count += 1
-        end
-        assert count > 0
-      end
-            
-      context "class names" do
-        setup do
-          @class_names = @client.class_names(@op, Sfcc::Flags::DeepInheritance)
-        end
-        
-        should "be a Cimc::Enumeration" do
-          assert_kind_of(Sfcc::Cim::Enumeration, @class_names)
-        end
-        
-        should "include CIM_ManagedElement" do
-          assert !@class_names.select { |x| x.to_s == "CIM_ManagedElement" }.empty?
-        end
-        
-        should "have every element of type Sfcc::Cim::ObjectPath" do
-          @class_names.each { |n| assert_kind_of(Sfcc::Cim::ObjectPath, n) }
-        end            
+      it "should include CIM_ManagedElement" do
+        assert !@class_names.select { |x| x.to_s == "CIM_ManagedElement" }.empty?
       end
       
-      context "classes" do
-        setup do
-          @classes = @client.classes(@op, Sfcc::Flags::DeepInheritance)
-        end
-        
-        should "be a Cimc::Enumeration" do
-          assert_kind_of(Sfcc::Cim::Enumeration, @classes)
-        end
-        
-        should "have every alement of type Cimc::Class" do
-          @classes.each { |c| assert_kind_of(Sfcc::Cim::Class, c) }
-        end            
-      end
-      
+      it "should have every element of type Sfcc::Cim::ObjectPath" do
+        @class_names.each { |n| assert_kind_of(Sfcc::Cim::ObjectPath, n) }
+      end            
     end
-  end  
+    
+    describe "classes" do
+      before do
+        @classes = @client.classes(@op, Sfcc::Flags::DeepInheritance)
+      end
+      
+      it "should be a Cimc::Enumeration" do
+        assert_kind_of(Sfcc::Cim::Enumeration, @classes)
+      end
+      
+      it "should have every alement of type Cimc::Class" do
+        @classes.each { |c| assert_kind_of(Sfcc::Cim::Class, c) }
+      end            
+    end
+    
+  end
 end  
+
