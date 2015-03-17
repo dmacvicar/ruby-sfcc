9aa43ffe0a5f80c00e8c03c7a17340a8a98433c6
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-03-17 13:10:49 +0100

    port to minitest/spec

--- test/test_sfcc_cim_enumeration.rb
+++ test/test_sfcc_cim_enumeration.rb
@@ -1,39 +1,38 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
-#require 'pp'
+require_relative 'helper'
 
-class SfccCimEnumerationTest < SfccTestCase
+describe "enumeration of instances" do
+  include SfccTestUtils
 
-  context "enumeration of instances" do
-    setup do
-      setup_cim_client
-      op = Sfcc::Cim::ObjectPath.new("root/cimv2")
-      @enm = @client.query(op, "select * from CIM_ComputerSystem", "wql")
-    end
-    
-    should "be running" do
+  before do
+    setup_cim_client
+    op = Sfcc::Cim::ObjectPath.new("root/cimv2")
+    @enm = @client.query(op, "select * from CIM_ComputerSystem", "wql")
+  end
+  
+  it "should be running" do
       assert cimom_running?
-    end
+  end
+  
+  it "should be an enumeration" do
+    assert_kind_of(Sfcc::Cim::Enumeration, @enm)
+  end
+  
+  it "should be able to iterate twice" do
+    assert !@enm.to_a.empty?
+    assert !@enm.to_a.empty?
+  end
+  
+  it "should have a size" do
+    assert @enm.size > 0
+  end
+  
+  it "should have a simple type" do
+    assert @enm.simple_type > 0
+  end
+  
+  it "should has an array representation" do
+    assert_kind_of(Array, @enm.to_a)
+  end
+end   
 
-    should "be an enumeration" do
-      assert_kind_of(Sfcc::Cim::Enumeration, @enm)
-    end
-
-    should "be able to iterate twice" do
-      assert !@enm.to_a.empty?
-      assert !@enm.to_a.empty?
-    end
-
-    should "have a size" do
-      assert @enm.size > 0
-    end
-
-    should "have a simple type" do
-      assert @enm.simple_type > 0
-    end
-
-    should "has an array representation" do
-      assert_kind_of(Array, @enm.to_a)
-    end
-  end   
-end            
 
