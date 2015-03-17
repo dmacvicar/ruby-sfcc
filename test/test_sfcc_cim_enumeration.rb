3437ba02a9faf43037cc3f21960444195fa72fe1
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-03-17 13:11:13 +0100

    tests: rubocop pass

--- test/test_sfcc_cim_enumeration.rb
+++ test/test_sfcc_cim_enumeration.rb
@@ -1,38 +1,36 @@
 require_relative 'helper'
 
-describe "enumeration of instances" do
+describe 'enumeration of instances' do
   include SfccTestUtils
 
   before do
     setup_cim_client
-    op = Sfcc::Cim::ObjectPath.new("root/cimv2")
-    @enm = @client.query(op, "select * from CIM_ComputerSystem", "wql")
+    op = Sfcc::Cim::ObjectPath.new('root/cimv2')
+    @enm = @client.query(op, 'select * from CIM_ComputerSystem', 'wql')
   end
-  
-  it "should be running" do
-      assert cimom_running?
+
+  it 'should be running' do
+    assert cimom_running?
   end
-  
-  it "should be an enumeration" do
+
+  it 'should be an enumeration' do
     assert_kind_of(Sfcc::Cim::Enumeration, @enm)
   end
-  
-  it "should be able to iterate twice" do
+
+  it 'should be able to iterate twice' do
     assert !@enm.to_a.empty?
     assert !@enm.to_a.empty?
   end
-  
-  it "should have a size" do
+
+  it 'should have a size' do
     assert @enm.size > 0
   end
-  
-  it "should have a simple type" do
+
+  it 'should have a simple type' do
     assert @enm.simple_type > 0
   end
-  
-  it "should has an array representation" do
+
+  it 'should has an array representation' do
     assert_kind_of(Array, @enm.to_a)
   end
-end   
-
-
+end
