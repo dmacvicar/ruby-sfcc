f21b1138ad4ca472d4671ad2e9299f170aa9be74
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-03-17 13:21:39 +0100

    port to minitest/spec

--- test/test_sfcc_cim_type.rb
+++ test/test_sfcc_cim_type.rb
@@ -1,36 +1,34 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
-#require 'pp'
+require_relative 'helper'
 
-class SfccCimcType < SfccTestCase
+describe "a Cim::Class representation" do
+  include SfccTestUtils
 
-  context "a Cim::Class representation" do
-    setup do
-      setup_cim_client
-      op = Sfcc::Cim::ObjectPath.new("root/cimv2", "CIM_ComputerSystem")
-      @cimclass = @client.get_class(op)
+  before do
+    setup_cim_client
+    op = Sfcc::Cim::ObjectPath.new("root/cimv2", "CIM_ComputerSystem")
+    @cimclass = @client.get_class(op)
+  end
+  
+  it "should provides Cim::Type for all its properties" do
+    @cimclass.each_property do |k, d|
+      assert_kind_of(Sfcc::Cim::Type, d.type)
     end
-    
-    should "provides Cim::Type for all its properties" do
-      @cimclass.each_property do |k, d|
-        assert_kind_of(Sfcc::Cim::Type, d.type)
+  end
+  
+  it "should have string and Integer representations of Cim::Type for all its properties" do
+    @cimclass.each_property do |k, d|
+      assert_kind_of(String, d.type.to_s)
+      assert_kind_of(Integer, d.type.to_i)
+      if d.type.to_s =~ /\[\]/
+        assert d.type.array?
+      else
+        assert !d.type.array?
+        assert d.type.integer? if d.type.to_s =~ /int/
+        assert d.type.real? if d.type.to_s =~ /real/
+        assert d.type.string? if d.type.to_s =~ /string/
       end
     end
-    
-    should "have string and Integer representations of Cim::Type for all its properties" do
-      @cimclass.each_property do |k, d|
-        assert_kind_of(String, d.type.to_s)
-        assert_kind_of(Integer, d.type.to_i)
-        if d.type.to_s =~ /\[\]/
-          assert d.type.array?
-        else
-          assert !d.type.array?
-          assert d.type.integer? if d.type.to_s =~ /int/
-          assert d.type.real? if d.type.to_s =~ /real/
-          assert d.type.string? if d.type.to_s =~ /string/
-        end
-      end
-    end
-    
   end
   
 end
+
